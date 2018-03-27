ba388c09736a68334bf6fea19648f76f4437d618
ss223m <ss223m@intl.att.com>
2018-03-27 18:56:30 +0300

    Svetlana: add classes Puzzle,Edge

---
+++ src/main/java/course/puzzle/Edge.java
@@ -0,0 +1,11 @@
+package course.puzzle;
+
+public class Edge {
+    private String name;
+    private Integer value;
+
+    public Edge(String name, Integer value) {
+        this.name = name;
+        this.value = value;
+    }
+}
--- src/test/java/course/puzzle/PuzzleTest.java
+++ src/test/java/course/puzzle/PuzzleTest.java
@@ -1,12 +1,34 @@
 package course.puzzle;
 
-import org.junit.jupiter.api.Test;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
 
 public class PuzzleTest {
 
     @Test
-    public void testWrongNumberOfStraitEdges(){
-        PuzzlePiece p1 = new PuzzlePiece(1,)
+    public void badTestNumberOfStraightEdges(){
+        List<PuzzlePiece> puzzleList = new ArrayList<PuzzlePiece>();
+        PuzzlePiece p1 = new PuzzlePiece(1,1,1,1,1);
+        puzzleList.add(p1);
+         Puzzle bad = new Puzzle(puzzleList);
+
+        assertFalse(bad.validateNumberOfStraightEdges(1,1));
+    }
+
+    @Test
+    public void goodTestNumberOfStraightEdges(){
+        List<PuzzlePiece> puzzleList = new ArrayList<PuzzlePiece>();
+        PuzzlePiece p1 = new PuzzlePiece(1,0,1,1,1);
+        puzzleList.add(p1);
+        Puzzle good = new Puzzle(puzzleList);
+
+        assertTrue(good.validateNumberOfStraightEdges(1,1));
     }
 
 }
